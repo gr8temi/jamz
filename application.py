c0a8cadb70625951026f682bca318a31234fa344
cchigo <chigorgor@Chigorgor.localdomain>
2019-09-03 21:19:10 +0100

    connect validation logic to frontend

--- application.py
+++ application.py
@@ -1,17 +1,42 @@
 import os
-from cs50 import SQL
-from flask import Flask, jsonify, redirect, render_template, request, session
-from flask_session import Session
 
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
+from helpers import apology, login_required
 
-
+# Configure application
 app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Configure session to use filesystem i.e, local files (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+
 
 #login
 db = SQL("sqlite:///jamz.db")
+
+
 @app.route("/signup", methods=["GET","POST"])
 def signup():
     if request.method == "POST":
@@ -19,7 +44,7 @@ def signup():
         email = request.form.get("email")
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
- 
+  
         if not name:
             return apology ("name")  # error message
         
@@ -27,9 +52,49 @@ def signup():
             return apology("username not provided", 406)
         
         if (confirmation != password):
-            return apology("Password Mismatch")
+            return apology("Password Mismatch", 406)
+            
         hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
 
         db.execute("INSERT INTO users (name,email,hash) VALUES (:name, :email, :hash)", name=name, email=email, hash=hash) 
+        return redirect("/login")
     else:
-        return redirect("/")   
+        return render_template("signup.html")   
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("email"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+        
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE email = :email",
+                          email=request.form.get("email"))
+        print(rows[0]["hash"])
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return render_template("index.html")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
